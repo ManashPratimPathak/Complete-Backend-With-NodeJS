e5fbcff6bd40c9015a0e5bd269f1c321a47f0b3b
Manash Pratim Pathak <mppoff021@gmail.com>
2022-10-20 16:10:46 +0530

    learnt about helper function for navigation

--- 13_express_router/app.js
+++ 13_express_router/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -16,4 +15,4 @@ app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
 
- app.listen(3000);
+ app.listen(4000);
--- 13_express_router/routes/shop.js
+++ 13_express_router/routes/shop.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-
+const rootDir = require("../util/path")
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.sendFile(path.join(__dirname,"../" ,"views", "shop.html")); // res.sendFile('/views/shop.html'); this doesnt work because / here refer to root folder of our computer
+    res.sendFile(path.join(rootDir,"views", "shop.html")); // res.sendFile('/views/shop.html'); this doesnt work because / here refer to root folder of our computer
 });
 //__dirname is the global variable defined by nodejs that holds tha absolute path of our operating system to our project folder
 module.exports = router;
