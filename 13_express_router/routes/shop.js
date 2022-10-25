6334901cbdf8f44063a0bc86f16667ec7b20155b
Manash Pratim Pathak <mppoff021@gmail.com>
2022-10-25 19:12:33 +0530

    sharing data across request and users

--- 13_express_router/routes/shop.js
+++ 13_express_router/routes/shop.js
@@ -2,8 +2,12 @@ const express = require("express");
 const path = require("path");
 const rootDir = require("../util/path")
 const router = express.Router();
+const adminData = require("./admin");
+
+
 
 router.get('/', (req, res, next)=>{
+    console.log("shop.js "+ adminData.products);
     res.sendFile(path.join(rootDir,"views", "shop.html")); // res.sendFile('/views/shop.html'); this doesnt work because / here refer to root folder of our computer
 });
 //__dirname is the global variable defined by nodejs that holds tha absolute path of our operating system to our project folder
