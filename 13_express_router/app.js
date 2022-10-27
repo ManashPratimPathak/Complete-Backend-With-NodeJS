3bcac3cb82cdba4624b54b6c2b80bccdbd1d2c22
Manash Pratim Pathak <mppoff021@gmail.com>
2022-10-27 19:54:55 +0530

    added nodemon and implemented pug

--- 13_express_router/app.js
+++ 13_express_router/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-app.set("view-engine", "pug");
+app.set("view engine", "pug");
 app.set("views", "views"); 
 
 const adminData = require("./routes/admin");
@@ -11,7 +11,7 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminData.routes);
+app.use("/admin", adminData.routes); 
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
--- 13_express_router/routes/shop.js
+++ 13_express_router/routes/shop.js
@@ -7,8 +7,10 @@ const adminData = require("./admin");
 
 
 router.get('/', (req, res, next)=>{
-    console.log("shop.js "+ adminData.products);
-    res.sendFile(path.join(rootDir,"views", "shop.html")); // res.sendFile('/views/shop.html'); this doesnt work because / here refer to root folder of our computer
+    const products = adminData.products;
+    // console.log("shop.js "+ adminData.products);
+    // res.sendFile(path.join(rootDir,"views", "shop.html")); // res.sendFile('/views/shop.html'); this doesnt work because / here refer to root folder of our computer
+    res.render('shop', {prods: products, docTitle: "Shop"});
 });
 //__dirname is the global variable defined by nodejs that holds tha absolute path of our operating system to our project folder
 module.exports = router;
