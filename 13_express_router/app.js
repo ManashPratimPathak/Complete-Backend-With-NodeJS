74f63e64a157db550031bf68b32cd88fb1c0ab6c
Manash Pratim Pathak <mppoff021@gmail.com>
2022-10-27 23:07:30 +0530

    converted existing html files into pug

--- 13_express_router/app.js
+++ 13_express_router/app.js
@@ -15,7 +15,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404",{docTitle: "Page not found"});
+    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
 
  app.listen(4000);
